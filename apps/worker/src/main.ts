7bf4b0ec9bc3bb967b63d26f6a42075fcb9bf9ae
Ginks <pissabruney@hotmail.com>
2026-07-26 21:28:25 +0200

    keep production worker alive

--- apps/worker/src/main.ts
+++ apps/worker/src/main.ts
@@ -12,9 +12,12 @@ console.log('[worker] Black Whale worker starting...')
 // TODO: initialize job queues (Bull/BullMQ)
 // TODO: register processors
 
+const keepAlive = setInterval(() => {}, 60_000)
+
 await new Promise<void>((resolve) => {
   process.once('SIGTERM', resolve)
   process.once('SIGINT', resolve)
 })
 
+clearInterval(keepAlive)
 console.log('[worker] Shutting down gracefully...')

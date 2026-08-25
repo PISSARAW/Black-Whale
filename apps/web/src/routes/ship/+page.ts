0feffdbe54ab0de99258d03b6ed27f0ecf20bc91
Ginks <pissabruney@hotmail.com>
2026-08-25 11:22:29 +0200

    fix(web): drop the placeholder universal load on /ship that faked an unused mapState field

--- apps/web/src/routes/ship/+page.ts
+++
@@ -1,6 +0,0 @@
-import type { PageLoad } from './$types'
-
-export const load: PageLoad = async ({ data }) => {
-  // TODO: load map state for current chapter
-  return { ...data, mapState: null }
-}
